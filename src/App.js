cd400565e1325384d3e05d51615596404d1c1a47
ashish44khanal <ashish24khanal@gmail.com>
2022-11-10 19:52:05 +0545

    initial repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState([])
+  console.log("apiresdata",data)
+  const fetchData=async()=>{
+    const res=await fetch(`https://jsonplaceholder.typicode.com/users`)
+    console.log(res)
+    const resData=await res.json()
+    
+    setData(resData)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='text-gray-900 '>
+    {data.map((item)=>(
+      <div key={item.id} className='border m-8'>
+        <p>NAME : {item.name}</p>
+        <p>CITY : {item.address.city}</p>
+        </div>
+    ))}
+   </div>
   );
 }
 
